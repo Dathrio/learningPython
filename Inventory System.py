0d1b9028cc91c50fbbbc1a35c69aab93b1a83fd3
Kevin Griffith <kevingriffith.kg@gmail.com>
2022-01-04 14:43:35 -0800

    Added "add to invetnory function" to increase values in the dictionary or add new keys, when picking up items

--- Inventory System.py
+++ Inventory System.py
@@ -1,7 +1,9 @@
 #RPG Inventory System
-
 #current items in bag
-bag = {'rope': 1, 'torch': 6, 'gold': 42, 'dagger': 1, 'arrow': 12}
+bag = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+#loot dropped by a dragon
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'ruby']
 
 #go through each item in your inventory to display the contents and count total items
 def displayInventory(inventory):
@@ -14,4 +16,17 @@ def displayInventory(inventory):
         totalNumber += v
     print("Total number of items: " + str(totalNumber))
 
+#add loot to current inventory, updating count of existing items and adding new items to dictionary
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        if not item in inventory:
+            inventory[item] = 1
+        else:
+            inventory[item] += 1
+    print("Invetory Updated")
+
+#display initial inventory
+displayInventory(bag)
+#add the loot and redisplay inventroy
+addToInventory(bag, dragonLoot)
 displayInventory(bag)
